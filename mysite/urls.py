a7277b813fe01d5d6c52369904361e11436574eb
eugene <7777eugene@gmail.com>
2025-06-02 16:49:11 +0900

    final No1 project of polls

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
+# 라우터 연결
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("polls/", include("polls.urls")),
--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
 
+# 함수, 클래스 연결
+
+app_name = "polls" # 네임스페이스 지정
+
 urlpatterns = [
     path("", views.index, name="index"),
-
+    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:question_id>/results/", views.results, name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
