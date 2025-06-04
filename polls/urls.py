2fc391b156bbfb9f14a057ebfb1f9a6a7bf76bbe
eugene <7777eugene@gmail.com>
2025-06-04 14:06:14 +0900

    [feat 장고 part4]

--- polls/urls.py
+++ polls/urls.py
@@ -6,8 +6,14 @@ from . import views
 app_name = "polls" # 네임스페이스 지정
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("<int:question_id>/", views.detail, name="detail"),
-    path("<int:question_id>/results/", views.results, name="results"),
+    # path("", views.index, name="index"),
+    # path("<int:question_id>/", views.detail, name="detail"),
+    # path("<int:question_id>/results/", views.results, name="results"),
+    # path("<int:question_id>/vote/", views.vote, name="vote"),
+
+    # CBV 방식으로 변경
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,55 +1,97 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import Question
+# from django.http import HttpResponse
+from .models import Question, Choice
+from django.views import generic
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.db.models import F
 
-def index(request):
-    # return HttpResponse("Hello, world. You're at the polls index.")
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
 
-def detail(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, "polls/detail.html", {"question": question})
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+    # context_object_name = "question"
 
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+    # context_object_name = "question"
+
+# 투표 처리 로직
 def vote(request, question_id):
-    return HttpResponse(f"You're voting on question {question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message":"You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
 
-    html = """
-    <html>
-    <head>
-        <title>투표 사이트</title>
-        <style>
-            body {
-                font-family: Arial, sans-serif;
-                background-color: #f2f2f2;
-                padding: 2em;
-            }
-            h1 {color: #333399;}
-            .box {
-                background: white;
-                padding: 1.5em;
-                border-radius: 10px;
-                box-shadow: 0 0 10px rgba(0,0,0,0.1);
-                max-width: 600px;
-                margin: auto;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="box">
-            <h1>안녕하세요!</h1>
-            <p>여기는 <strong>설문조사(polls)</strong> 
-            사이트의 메인 페이지입니다.
-            </p>
-            <p>관리자는 설문을 추가하고, 사용자는 투표할 수 있습니다.</p>
-        </div>
-    </body>
-    </html>
-    """
-    return HttpResponse(html)
-# Create your views here.
+
+
+# def index(request):
+#     # return HttpResponse("Hello, world. You're at the polls index.")
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     context = {"latest_question_list": latest_question_list}
+#     return render(request, "polls/index.html", context)
+
+# def detail(request, question_id):
+# 	question = get_object_or_404(Question, pk=question_id)
+# 	return render(request, "polls/detail.html", {"question": question})
+
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/results.html", {"question": question})
+
+# def vote(request, question_id):
+#     return HttpResponse(f"You're voting on question {question_id}.")
+
+#     html = """
+#     <html>
+#     <head>
+#         <title>투표 사이트</title>
+#         <style>
+#             body {
+#                 font-family: Arial, sans-serif;
+#                 background-color: #f2f2f2;
+#                 padding: 2em;
+#             }
+#             h1 {color: #333399;}
+#             .box {
+#                 background: white;
+#                 padding: 1.5em;
+#                 border-radius: 10px;
+#                 box-shadow: 0 0 10px rgba(0,0,0,0.1);
+#                 max-width: 600px;
+#                 margin: auto;
+#             }
+#         </style>
+#     </head>
+#     <body>
+#         <div class="box">
+#             <h1>안녕하세요!</h1>
+#             <p>여기는 <strong>설문조사(polls)</strong> 
+#             사이트의 메인 페이지입니다.
+#             </p>
+#             <p>관리자는 설문을 추가하고, 사용자는 투표할 수 있습니다.</p>
+#         </div>
+#     </body>
+#     </html>
+#     """
+#     return HttpResponse(html)
+# # Create your views here.
